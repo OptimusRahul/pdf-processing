7638b244ee3a5db2bfbe204943aad2cddc905d50
OptimusRahul <rahul@sparrowmarketing.com>
2024-06-09 02:39:12 +0530

    enhancement(pdf): added support for password protected pdf and fixed canvas issue for some pdf where pdfjs-dist is failing to render in web-worker

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React, { useState, ChangeEvent, useEffect } from 'react';
+import React, { useState, ChangeEvent, useEffect, useCallback } from 'react';
 
 import { PDFViewer } from './pdf-viewer';
 import './App.css';
+
+import * as pdfjsLib from "pdfjs-dist";
+import { WorkerMessageHandler } from "pdfjs-dist/build/pdf.worker.mjs";
+
+pdfjsLib.GlobalWorkerOptions.workerSrc = WorkerMessageHandler;
 
 const worker = new Worker(new URL('./pdf-worker.js', import.meta.url), { type: 'module' });
 
@@ -9,8 +14,55 @@ const App: React.FC = () => {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
   const [pageBase64, setPageBase64] = useState<string[]>([]);
 
+  const handleFallback = useCallback(async (pageIndex: number) => {
+    const arrayBuffer = await (selectedFile as File).arrayBuffer();
+
+    const pdfDoc = await pdfjsLib.getDocument({ data: arrayBuffer }).promise;
+    const page = await pdfDoc.getPage(pageIndex);
+    const viewport = page.getViewport({ scale: 1.3 });
+
+    const canvas = document.createElement("canvas");
+    const context = canvas.getContext("2d");
+
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+
+    await page.render({ canvasContext: context!, viewport }).promise;
+
+    canvas.toBlob((blob) => {
+      worker.postMessage({ type: "renderPageResult", pageIndex, blob });
+    }, "image/png");
+
+  }, [selectedFile])
+
+  const handlePasswordException = useCallback(async () => {
+    const password = prompt("This PDF is password protected. Please enter the password:");
+    if (password) {
+      const buffer = await (selectedFile as File).arrayBuffer();
+      worker.postMessage({ arrayBuff: buffer, password });
+    }
+  }, [selectedFile])
+
   useEffect(() => {
-    worker.onmessage = (e) => {
+    worker.onmessage = async (e) => {
+
+      if (e.data.type === "renderPage") {
+        const { pageIndex } = e.data;
+        handleFallback(pageIndex);
+        return;
+      }
+      
+      if (e.data.type === "needPassword") {
+        handlePasswordException();
+        return
+      } 
+
+      if (e.data.type === "error") {
+        console.error("Error in PDF processing:", e.data.error);
+        prompt('Error in PDF processing.')
+        return;
+      }
+
       const { images = [], start, size } = e.data;
 
       // Early return if size is not defined
@@ -27,18 +79,18 @@ const App: React.FC = () => {
         return updatedArray;
       });
     };
-  }, []);
+  }, [handleFallback, handlePasswordException, selectedFile]);
 
-  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleFileChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files) {
       setSelectedFile(event.target.files[0]);
     }
-  };
+  }, [])
 
-  const handleFileUpload = async () => {
+  const handleFileUpload = useCallback(async() => {
     const buffer = await (selectedFile as File).arrayBuffer();
     worker.postMessage({ arrayBuff: buffer });
-  };
+  }, [selectedFile])
 
   return (
     <div className="App">
